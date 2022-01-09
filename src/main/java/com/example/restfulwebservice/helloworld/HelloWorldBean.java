3e1825ccf5f36dc256f986fdac533e2d47364826
Seunghun Lee <hun2456@hanmail.net>
2022-01-09 22:08:39 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/restfulwebservice/helloworld/HelloWorldBean.java
@@ -0,0 +1,12 @@
+package com.example.restfulwebservice.helloworld;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class HelloWorldBean {
+
+    private String message;
+
+}
